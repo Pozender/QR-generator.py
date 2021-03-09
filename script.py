5a3fe6caaa2d3426bd633ab84eb284d6e7b60659
Pozender <66801105+Pozender@users.noreply.github.com>
2021-03-09 23:56:38 +0100

    Add files via upload

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 import qrcode
+import os
+import shutil
 
 #inputs
 objectToEncode = input("Rentrez votre Url: ")
@@ -8,6 +10,7 @@ targetName = input("Entrez le nom de sortie du fichier: ")
 PNG = targetName + ".png"
 SVG = targetName + ".svg"
 JPG = targetName + ".jpg"
+formats = [PNG, SVG, JPG]
 
 #Qr personalisation
 qr= qrcode.QRCode(
@@ -25,6 +28,28 @@ qr.make(fit=True)
 img = qr.make_image(fill_color= "black", back_color= "white")
 
 #output file
-img.save(PNG)
-img.save(SVG)
-img.save(JPG)
+try:
+    os.mkdir(targetName)
+except:
+    folderexist = input("Ce fichier existe déjà veuillez le suprimmer, voulez vous le faire maintenant [Y/N]: ")
+    if folderexist == "Y" or folderexist == "y":
+        shutil.rmtree(targetName)
+        print("nous venons de supprimer le dossier nous allons le recréer")
+        os.mkdir(targetName)
+    elif folderexist == "N" or folderexist == "n":
+        print("Vous devez impérativement supprimer le dossier manuellement et relancer le programe pour le bon déroulement de celui-ci")
+        breakpoint
+
+for i in formats:
+    
+    #all create a file in a format of the list
+    img.save(i)
+
+    source = i
+    target = targetName
+    
+    #copy the source in the target
+    shutil.copy(source, target)
+
+    #remove the source
+    os.remove(source)
